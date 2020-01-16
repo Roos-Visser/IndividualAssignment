9e8e6917a334a7b56116aab8319ec1c9214b8447
Roos Visser <roos-visser@hotmail.com>
2020-01-16 14:46:55 +0100

    Added comments to python code

--- IndividualAssignment.py
+++ IndividualAssignment.py
@@ -1,10 +1,12 @@
 import json
 
+# Open json file with data
 with open('Bosnia1993Data.json', encoding='utf-16') as file:
     data = json.load(file)
 
 event = data[0]
 
+# Convert json file to a csv file with header and only useful columns
 with open('Bosnia1993Data.csv', 'w', encoding='utf8') as file:
     file.write(f"best_estimate, highest_reliable_estimate, lowest_reliable_estimate\n")
     for event in data:
