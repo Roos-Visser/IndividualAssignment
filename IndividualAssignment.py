ae2031ac37381a96bbc29562304918b6572729ae
Roos Visser <roos-visser@hotmail.com>
2020-01-16 14:21:56 +0100

    Created a temporary csv file with one of the data entries + added headers to columns

--- IndividualAssignment.py
+++ IndividualAssignment.py
@@ -2,3 +2,10 @@ import json
 
 with open('Bosnia1993Data.json', encoding='utf-16') as file:
     data = json.load(file)
+
+event = data[0]
+
+with open('Bosnia1993Data.csv', 'w', encoding='utf8') as file:
+    file.write(f"best_estimate, highest_reliable_estimate, lowest_reliable_estimate\n")
+    file.write(f"{event['best']}, {event['high']}, {event['low']}")
+    
