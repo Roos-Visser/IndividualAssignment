ef0729c15c993ce49b96ef6dfffbcbdeaa4b15dd
Roos Visser <roos-visser@hotmail.com>
2020-01-17 10:14:16 +0100

    Conversion csv done with Python

--- IndividualAssignment.py
+++ IndividualAssignment.py
@@ -1,14 +1,14 @@
 import json
 
 # Open json file with data
-with open('Bosnia1993Data.json', encoding='utf-16') as file:
+with open('DatasetFinal.json', encoding='utf-16') as file:
     data = json.load(file)
 
 event = data[0]
 
 # Convert json file to a csv file with header and only useful columns
-with open('Bosnia1993Data.csv', 'w', encoding='utf8') as file:
-    file.write(f"best_estimate, highest_reliable_estimate, lowest_reliable_estimate\n")
+with open('DatasetFinal.csv', 'w', encoding='utf8') as file:
+    file.write(f"year, best_estimate, highest_reliable_estimate, lowest_reliable_estimate\n")
     for event in data:
-        file.write(f"{event['best']}, {event['high']}, {event['low']}\n")
+        file.write(f"{event['year']}, {event['best']}, {event['high']}, {event['low']}\n")
     
